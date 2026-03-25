a0b4328ef4491f8c560e04aec25a4dbd3743107f
veebaba <34164890+veebaba@users.noreply.github.com>
2026-03-25 11:43:03 -0400

    create security group

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_security_group" "blog" {
 resource "aws_security_group_rule" "blog_http_in" {
   type           = "ingress"
   from_port      = 80
-  to_port.       = 80
+  to_port       = 80
   protocol       = "tcp"
   cidr_blocks    = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
