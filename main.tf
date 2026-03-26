4fa422c204aab54768da2cb43f3a0da8e8d15b8e
veebaba <34164890+veebaba@users.noreply.github.com>
2026-03-26 07:48:39 -0400

    created a new dev vpc

--- main.tf
+++ main.tf
@@ -15,14 +15,35 @@ data "aws_ami" "app_ami" {
 }
 
 
-data "aws_vpc" "default" {
-  default = true
+
+#dev vpc
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
+
 
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
@@ -40,43 +61,8 @@ module "blog_sg" {
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules        = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 }
 
-
-resource "aws_security_group" "blog" {
-  name = "blog"
-  description = "Allow http and https in. Allow everything out"
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type           = "ingress"
-  from_port      = 80
-  to_port       = 80
-  protocol       = "tcp"
-  cidr_blocks    = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
-}
