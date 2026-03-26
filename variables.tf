3cd663ce6db786f4284a69c9dabf477065322f6f
veebaba <34164890+veebaba@users.noreply.github.com>
2026-03-26 11:17:52 -0400

    refactor to use variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,42 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+
+
+variable "ami_filter" {
+  description = "Name filter and owner for AMI"
+  type = object ({
+    name   = string
+    object = string
+  })
+
+  default =  {
+    name   = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner  = "979382823631" # Bitnami
+  }
+}
+
+
+variable "environment" {
+  description = "deployment environment"
+  type = object ({
+    name           = string
+    network_prefix = string
+
+  default = {
+    name           = "dev"
+    network_prefix = "10.0"
+  } 
+  })
+}
+
+variable "min_size" {
+  description  = "minimum number of instances in the asg"
+  default      = 1
+}
+
+variable "max_size" {
+  description = "maximum number of instances in the asg"
+  default     = 2
+}
