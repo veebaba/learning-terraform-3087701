3558dd4598f6c1595102224ba3fa886ea9095c63
veebaba <34164890+veebaba@users.noreply.github.com>
2026-03-26 09:23:37 -0400

    auto scaling

--- main.tf
+++ main.tf
@@ -39,18 +39,6 @@ module "blog_vpc" {
 
 
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -101,9 +89,30 @@ resource "aws_lb_target_group" "blog" {
   vpc_id   = module.blog_vpc.vpc_id
 }
 
-resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = aws_lb_target_group.blog.arn
-  target_id        = aws_instance.blog.id
-  port             = 80
+
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.2.0"
+
+
+  name     = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier  = module.blog_vpc.public_subnets
+
+  launch_template_name = "blog"
+
+  security_groups      = [module.blog_sg.security_group_id]
+
+  image_id             = data.aws_ami.app_ami.id
+  instance_type        = var.instance_type
+
+  traffic_source_attachments = {
+    blog-alb = {
+      traffic_source_identifier = aws_lb_target_group.blog.arn
+    }
+  }
+
 }
 
