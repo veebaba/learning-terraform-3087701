0c5f605ec0b6b5afb43ca8e99bb3ec9400315e3e
veebaba <34164890+veebaba@users.noreply.github.com>
2026-03-26 08:22:40 -0400

    load balancer

--- main.tf
+++ main.tf
@@ -66,3 +66,44 @@ module "blog_sg" {
   vpc_id = module.blog_vpc.vpc_id
 }
 
+
+
+module "blog_alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = [module.blog_vpc.public_subnets]
+  security_groups = [module.blog_sg.security_group_id]
+
+
+  listeners = {
+    blog_http = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_arn = aws_lb_target_group.blog.arn
+
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+
+resource "aws_lb_target_group" "blog" {
+  name     = "blog"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.blog_vpc.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "blog" {
+  target_group_arn = aws_lb_target_group.blog.arn
+  target_id        = aws_instance.blog.id
+  port             = 80
+}
+
